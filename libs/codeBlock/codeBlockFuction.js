f6f481072636ab9f1fa45b6122e9e9a8ee37fd41
atmujie <2633298206@qq.com>
2021-06-06 10:39:04 +0800

    Site updated: 2021-06-06 10:38:54

--- libs/codeBlock/codeBlockFuction.js
+++ libs/codeBlock/codeBlockFuction.js
@@ -1,5 +1,9 @@
 // 代码块功能依赖
 
+// $(function () {
+//     $('pre').wrap('<div class="code-area" style="position: relative"></div>');
+// });
+// 代码块功能依赖
 $(function () {
     $('pre').wrap('<div class="code-area" style="position: relative"></div>');
 });
